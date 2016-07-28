06e7c8a37036cc982d3f89e24bc93088b597c081
Anthony Salani <asalani93@gmail.com>
2016-07-28 11:47:11 -0400

    adds documentation

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const express = require('express');
 const consolidate = require('consolidate');
 const TimesController = require('./controllers/times');
 
+// Typical express app initialization
 const app = express();
 
 app.use(express.static('views'));
 app.use('/assets', express.static('assets'));
 
+// Attach the (only) controller to the app
 const timesController = new TimesController(app);
 
+// Start listening for people interested in train times
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
